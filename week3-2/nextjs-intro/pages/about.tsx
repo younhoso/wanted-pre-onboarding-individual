f71b03f5edefc64c23e7a1c672cfa56e7550886c
younhoso <thdbsgh3443@naver.com>
2022-11-13 20:25:49 +0900

    nextJS 세팅

---
+++ week3-2/nextjs-intro/pages/about.tsx
@@ -0,0 +1,10 @@
+import NavBar from '../components/NevBar'
+
+export default function Potato () {
+	return (
+		<div>
+			<NavBar />
+			<div>about</div>
+		</div>
+	)
+}
---
+++ week3-2/nextjs-intro/pages/index.tsx
@@ -0,0 +1,10 @@
+import NavBar from '../components/NevBar'
+
+export default function Home() {
+	return (
+		<div>
+			<NavBar />
+			<h1>Home</h1>
+		</div>
+	);
+}
